e33e6bd9c575cee07d7312694dd69745b0c878a7
PRAKASH8922 <143700371+PRAKASH8922@users.noreply.github.com>
2025-01-07 22:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ setTimeout(()=>{
 },1500);
 
 // Initialize EmailJS
-emailjs.init("6Y7yMDTb9ucDogH0j"); // Replace with your actual public key
+emailjs.init("m-dokqhPUQxNrWsLX"); // Replace with your actual public key
 
 // Select the form using the class name
 //const form = document.querySelector(".contact-form");
